a5bcc3986e6a8698f3f4b60fea127779f90a79e3
wellyandriani <wellyandriani97@gmail.com>
2018-12-03 14:27:49 +0700

    updateee again

---
+++ sticky/index.js
@@ -0,0 +1,7 @@
+
+var sidebar = new StickySidebar('#sidebar', {
+    containerSelector: '#main-content',
+    innerWrapperSelector: '.sidebar__inner',
+    topSpacing: 20,
+    bottomSpacing: 20
+});
