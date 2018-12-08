a29d31ad6bd6b1b784245b91304df24282d6c346
wellyandriani <wellyandriani97@gmail.com>
2018-12-08 09:28:59 +0700

    swetalert2

--- sweetAlert2/index.js
+++ sweetAlert2/index.js
@@ -1,55 +1,16 @@
-$('#bt1').click(function(){
+function normal() {
+  alert("Normal Alert!");
+  }
+  function sweet(){
+  swal("Good job!", "You clicked the button!", "success");
+  }
+
   
-    swal("Hello world!");
+  function swal2(){
+  swal({
+      title: 'Oops...',
+      text: 'Something went wrong!'
+    })
+
+  }
     
-  });
-  
-  $('#bt2').click(function(){
-    
-    swal("Oops...", "Something went wrong!", "error");
-    
-  });
-  
-  $('#bt3').click(function(){
-    
-    swal({
-    title: "Are you sure?",
-    text: "You will not be able to recover this imaginary file!",
-    type: "warning",
-    showCancelButton: true,
-    confirmButtonColor: "#DD6B55",
-    confirmButtonText: "Yes, delete it!",
-    closeOnConfirm: false,
-    html: false
-  }, function(){
-    swal("Deleted!",
-    "Your imaginary file has been deleted.",
-    "success");
-  });
-    
-  });
-  
-  $('#bt4').click(function(){
-    
-    swal({
-    title: "An input!",
-    text: "Write something interesting:",
-    type: "input",
-    showCancelButton: true,
-    closeOnConfirm: false,
-    animation: "slide-from-top",
-    inputPlaceholder: "Write something"
-  },
-  function(inputValue){
-    if (inputValue === false) return false;
-    
-    if (inputValue === "") {
-      swal.showInputError("You need to write something!");
-      return false
-    }
-    
-    swal("Nice!", "You wrote: " + inputValue, "success");
-  });
-    
-  });
-  
